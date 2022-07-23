fa5111ca89b046cf7263b122ae1c41f48d803020
Dwayne Jagernauth <dj7173994@gmail.com>
2022-07-23 18:57:45 -0400

    writing a function with a return value ussing modulus

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function(num) {
+
+  return num % 2 === 0;
+  
+}
+//const twentyThreeisEven = isEven(23);
+//const twentyFourisEven = isEven(24);
+console.log('Is 23 even? ' + isEven(23));
+console.log('Is 24 even? ' + isEven(24));
